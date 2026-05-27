7c9e2c85540e439b5736c0dc2f8775b4f404b9a2
Dinakar <dinakargaddam23@gmail.com>
2026-05-27 11:48:58 +0530

    Dynamic instance creation and assigning ip address andupating R53 rocords

--- roboshop.sh
+++ roboshop.sh
@@ -7,31 +7,31 @@ DOMAIN_NAME="dinakardevops.online"
 for instance in $@
 do 
     echo "Launching instance:$instance"
-    INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id ami-0220d79f3f480ecf5 \
-    --instance-type t3.micro \
-    --security-groups "roboshop-common" "roboshop=$instance" \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' \
-    --query 'Instances[0].InstanceId' \
+    INSTANCE_ID=$(aws ec2 run-instances\
+    --image-id ami-0220d79f3f480ecf5\
+    --instance-type t3.micro\
+    --security-groups "roboshop-common" "roboshop=$instance"\
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]"\
+    --query 'Instances[0].InstanceId'\
     --output text)
-    echo "Instance_id : $INSTANCE_ID"
+    echo "Instance_id:$INSTANCE_ID"
 
     if [ $instance="frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-            --query "Reservations[*].Instances[*].[PublicIpAddress]" \
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID\
+            --query "Reservations[*].Instances[*].[PublicIpAddress]"\
             --output text
         ) 
         R53_RECORD="$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID\
-          --query "Reservations[*].Instances[*].[PrivateIpAddress]" \
+          --query "Reservations[*].Instances[*].[PrivateIpAddress]"\
           --output text
         )
         R53_RECORD="$instance.$DOMAIN_NAME"
     fi
     #updating route53 rocord
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
+    aws route53 change-resource-record-sets\
+    --hosted-zone-id $ZONE_ID\
     --change-batch "
         {
                 "Comment": "Updating the IP address for production subdomain",
