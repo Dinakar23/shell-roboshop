decf51a88f58fc2e39c47551ada1cab909b7662b
Dinakar <dinakargaddam23@gmail.com>
2026-05-27 12:04:50 +0530

    Dynamic instance creation and assigning ip address andupating R53 rocords

--- roboshop.sh
+++ roboshop.sh
@@ -10,21 +10,21 @@ do
     INSTANCE_ID=$(aws ec2 run-instances\
     --image-id ami-0220d79f3f480ecf5\
     --instance-type t3.micro\
-    --security-groups "roboshop-common" "roboshop=$instance"\
+    --security-groups "roboshop-common" "roboshop-$instance"\
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]"\
     --query 'Instances[0].InstanceId'\
     --output text)
     echo "Instance_id:$INSTANCE_ID"
 
-    if [ $instance="frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID\
-            --query "Reservations[*].Instances[*].[PublicIpAddress]"\
+    if [ "$instance" = "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+            --query "Reservations[*].Instances[*].[PublicIpAddress]" \
             --output text
         ) 
         R53_RECORD="$DOMAIN_NAME"
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID\
-          --query "Reservations[*].Instances[*].[PrivateIpAddress]"\
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+          --query "Reservations[*].Instances[*].[PrivateIpAddress]" \
           --output text
         )
         R53_RECORD="$instance.$DOMAIN_NAME"
