170b10e6cdd225bf58febe45e5b45d6e6596d0c9
Dinakar <dinakargaddam23@gmail.com>
2026-05-28 19:39:51 +0530

    Roboshop V2

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -32,7 +32,7 @@ do
         if [ "$INSTANCE_ID" == "none" ]; then
             echo "Launching instance : roboshop-$instance"  
             INSTANCE_ID=$(aws ec2 run-instances \
-            --image-id ami-0220d79f3f480ecf5 \
+            --image-id AMI_ID \
             --instance-type t3.micro \
             --security-groups "roboshop-common" "roboshop-$instance" \
             --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
