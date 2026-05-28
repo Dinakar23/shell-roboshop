6519d90341458ea23d6139b7cfbf66d98c072fef
Dinakar <dinakargaddam23@gmail.com>
2026-05-28 17:45:28 +0530

    Roboshop V2

---
+++ roboshop-v2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z0522495674LVI9OE7CB"
+DOMAIN_NAME="dinakardevops.online"
+
+# validation
+if [ $# -ne 2 ]; then
+    echo -e "$R ERROR:: Atleast 2 arguments required $N"
+    ehco "USAGE : $0 [create/delete] [instace1] [instace2]"
+    exit 1
+fi
