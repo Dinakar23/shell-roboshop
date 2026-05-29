152c268fb322fb45c2f5daf36cb67ad3f339442c
Dinakar <dinakargaddam23@gmail.com>
2026-05-29 18:19:56 +0530

    Roboshop frontend added

--- 10-frontend.sh
+++ 10-frontend.sh
@@ -45,7 +45,7 @@ VALIDATE $? "Download and extracted frontend code"
 rm -rf /etc/nginx/nginx.conf
 VALIDATE $? "Removed defalut conf"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx/conf
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copied roboshop nginx conf"
 
 systemctl restart nginx 
